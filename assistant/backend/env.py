f6795770428e9cdb9aa2f27c2065b551cd1a28dc
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-12-30 19:45:28 -0600

    fix: remove logs

--- assistant/backend/env.py
+++ assistant/backend/env.py
@@ -1,13 +1,9 @@
-# Check if the environment is set up correctly
 def check_env():
     import sys
- 
     print("Running environment check inside Docker container.")
  
-    # List of required modules
     required_modules = ['connexion', 'uvicorn', 'sqlalchemy', 'alembic', 'aiosqlite']
     
-    # Check for required modules
     for module in required_modules:
         try:
             __import__(module)
